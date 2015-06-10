edbce35d9a12733c25c8398999d38260fba80d6b
nevin123 <nevin-wouters@hotmail.com>
2015-06-10 15:27:22 +0200

    Menu

---
+++ home.php
@@ -0,0 +1,9 @@
+<div class="page">
+	<div id="colors">
+		<div class="color 1"></div>
+		<div class="color 2"></div>
+		<div class="color 3"></div>
+		<div class="color 4"></div>
+		<div class="color 5"></div>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 	<meta charset="UTF-8">
 	<title>Odd Duck</title>
 	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div id="header">
-		<div id="logo"></div>
-		<div class="headerlinks"></div>
-		<div class="headerlinks"></div>
-		<div class="headerlinks"></div>
-		<div class="headerlinks"></div>
-	</div>
+
+	<?php require_once("menu.php"); ?>
+
+	<script src="js/jquery.js"></script>
+	<script src="js/js.js"></script>
 </body>
 </html>
