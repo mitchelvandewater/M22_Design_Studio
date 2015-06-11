4c7a0188b294c1a032e7bc015b7b813a8c50ca74
nevin123 <nevin-wouters@hotmail.com>
2015-06-11 09:55:56 +0200

    menu fix

--- about.php
+++ about.php
@@ -1,17 +1,5 @@
 <div class="page">
 
-	<div id="colors">
-		<div class="color c1"></div>
-		<div class="color c2"></div>
-		<div class="color c3"></div>
-		<div class="color c4"></div>
-		<div class="color c5"></div>
-	</div>
 	
-	<div class="home_logo">
-		<img src="images/logo.png" alt="The Odd Duck">
-
-		<div class="text">The <span class="white">Odd</span> Duck</div>
-	</div>
 	
 </div>
