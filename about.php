232346359d40568f3bcdb819c289402f7415bfab
mitchelvandewater <m.vandewater@student.fontys.nl>
2015-06-14 12:35:21 +0200

    Tekst filled

--- about.php
+++ about.php
@@ -5,10 +5,9 @@
 	</div>
 	<div class="tekstIntroductie"><h1> Odd Duck</h1>
 		<br>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat magni pariatur recusandae ut nam eligendi neque, facere quisquam, explicabo temporibus.</p>
-		<p>Ducimus maiores hic laborum, quia dicta tempora optio esse, reiciendis porro voluptatibus unde totam vero enim, iure quod minus eos.</p>
-		<p>Maxime, eligendi qui nesciunt nulla incidunt beatae aliquam itaque enim voluptates magni ipsum, repellendus, totam saepe ullam, vitae ducimus vel.</p>
-		<p>Quo optio tenetur, at veniam temporibus beatae nobis soluta a doloribus eius adipisci nisi amet reiciendis eos recusandae ullam minima.</p>
+		<p>Studio Odd Duck is een nieuwe beginnende studio die zich specialiseert in design van websites en huisstijlen. We gebruiken de laatste technologieën om de klant tevreden te houden. De klant en zijn idee staat bij ons op nummer één. </p>
+		<p>Odd Duck staat voor een krachtig design, toegepast binnen alledaagse bedrijven.</p>
+		<p>We streven er naar om de toegankelijkheid van grafisch ontwerp en technologieën beschikbaar te maken.</p>
 	</div>
 
 </div>
--- werknemers.php
+++ werknemers.php
@@ -6,7 +6,7 @@
 		<div class="werknemerimage"><img src="images/werknemers_1_mitchel.png" width="100%"alt=""></div>
 		<div class="werknemertext"><p><h3> Mitchel van de Water</h3></p>
 			<p><h4>Manager / Programmer</h4></p>
-			<p> Mitchel is een gemotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar</p>
+			<p>Mitchel regelt het klantencontact, maar denkt ook mee aan het creatieve proces. Bij de uitwerking helpt hij met programmeren. Door goed klantencontact en een leidinggevende rol krijgen de klanten zo een persoonlijke aanpak.</p>
 			</div>
 	</div>
 
@@ -16,14 +16,14 @@
 		<div class="werknemertext">
 			<p><h3> Rens Vlaar</h3></p>
 			<p><h4>Designer / Creative Writer</h4></p>
-			<p> Rens is een gemotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar</p>
+			<p>Rens is onze creatieve schrijver, gespecialiseerd in communicatie verwoord hij op een krachtige manier zijn gedachten. Hij zorgt voor een duidelijk beeld van wat de klant wilt bereiken.</p>
 		</div>
 	</div>
 	<div class="werknemerpersoon">
 		<div class="werknemerimage"><img src="images/werknemers_3_aylin.png" width="100%"alt=""></div>
 		<div class="werknemertext">			<p><h3> Aylin Nederlof</h3></p>
 			<p><h4>Designer / Creative Writer</h4></p>
-			<p> Rens is een gemotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar</p>
+			<p>Ayling specialiseert zich in Design, en werkt hier de bijbehorende producten bij uit. Ze leeft samen met alle werknemers het creatieve proces door en zorgt voor een  goed resultaat.</p>
 		</div>
 	</div>
 	<div class="werknemerpersoon">
@@ -31,7 +31,7 @@
 		<div class="werknemertext">
 						<p><h3> Nevin Wouters</h3></p>
 			<p><h4>Lead Programmer</h4></p>
-			<p> Nevin is een gemotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar</p>
+			<p> Nevin is onze lead programmer, met het toepassen van de nieuwe technologie zorgt hij voor een up-to-date resultaat wat bij de klant past. Bijblijvend met de tijd wordt er een technisch resultaat geleverd.</p>
 		</div>
 	</div>
 	<div class="werknemerpersoon">
@@ -39,7 +39,7 @@
 		<div class="werknemertext">
 						<p><h3> Steven van der Linden</h3></p>
 			<p><h4>Programmer / Typography</h4></p>
-			<p> Steven is een gemotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar emotiveerde student die klantencontact handeld Lorum Ipsum Locar</p>
+			<p>Steven is de man die zich specialiseert in typographie, als letterexpert weet hij altijd de juiste keuze te maken. Hij werkt ook aan het programeerproces. Door een creatieve match te vinden met de klant wordt altijd een persoonlijk resultaat opgeleverd</p>
 		</div>
 	</div>
 </div>
